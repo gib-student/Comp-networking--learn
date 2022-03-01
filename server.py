7abad554723b4e823c1f7d1d19fe3bff008ab51f
gib-student <gib19021@byui.edu>
2022-02-28 17:25:33 -0700

    Update

--- server.py
+++ server.py
@@ -17,23 +17,26 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        
-        print(str(addr) + " " + str(msg))
+        # Ensure message has content
+        if msg_length:    
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            
+            print(str(addr) + " " + str(msg))
+            conn.send("Msg received".encode(FORMAT))
         
     conn.close()
 
 def start():
     server.listen()
     print("[LISTENING] Server is listening on " + str(SERVER))
-    while True:
+    while True: 
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print("[ACTIVE CONNECTIONS] "  + str(threading.activeCount() - 1))
+        print("[ACTIVE CONNECTIONS] "  + str(threading.active_count() - 1))
 
 # Start the program
 print("[STARTING] server is starting...")
